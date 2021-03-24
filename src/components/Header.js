4c42498d2f0b364e663cfa8c6a88986dd26337b7
SHAW Bikash <bshaw@scorvelogica.com>
2021-03-24 16:32:47 -0400

    learning react

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,30 @@
-import PropTypes from 'prop-types';
-import Button from './Button'
+import PropTypes from "prop-types";
+import Button from "./Button";
+import { useLocation } from "react-router-dom";
 
-const Header = ({title}) => {
-    const onClick = (e) => {
-        console.log('Click!')
-    }
+const Header = ({ title, onAdd, showAdd }) => {
+  const location = useLocation();
 
-    return (
-        <header>
-            <h1 >{title}</h1>
-            <Button color='green' text='Add' onClick={onClick}></Button>
-           
-        </header>
-    )
-}
+  return (
+    <header>
+      <h1>{title}</h1>
+      {location.pathname === "/" && (
+        <Button
+          color={showAdd ? "red" : "green"}
+          text={showAdd ? "Close" : "Add"}
+          onClick={onAdd}
+        ></Button>
+      )}
+    </header>
+  );
+};
 
 Header.defaultProps = {
-    title: 'Task Tracker',
-}
+  title: "Task Tracker",
+};
 
 Header.propTypes = {
-    title: PropTypes.string.isRequired,
-}
+  title: PropTypes.string.isRequired,
+};
 
-// CSS in JS
-const headingStyle = {
-    color: 'red', 
-    backgroundColor: 'black'
-}
-
-export default Header
+export default Header;
